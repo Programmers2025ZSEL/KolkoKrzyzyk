2d59902ad6a997653235772819c4a7c46aa7df95
ZacnyZiom <hashziom3k@tutanota.com>
2025-11-18 19:07:01 +0100

    Next changes

--- Program.cs
+++ Program.cs
@@ -3,12 +3,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        PrintBoard();
+        int[,] board = new int[3, 3];
+        PrintBoard(board);
     }
 
-    private static void PrintBoard()
+    private static void PrintBoard(int[,] board)
     {
-        int[,] board = new int[3, 3];
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
